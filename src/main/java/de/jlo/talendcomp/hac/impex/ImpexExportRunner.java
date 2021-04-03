b5c46b5de0df111ceffca5c14a92bd7973b3f42e
Jan Lolling <jan.lolling@gmail.com>
2021-04-03 23:14:52 +0200

    Impex Export implemented

--- src/main/java/de/jlo/talendcomp/hac/impex/ImpexExportRunner.java
+++ src/main/java/de/jlo/talendcomp/hac/impex/ImpexExportRunner.java
@@ -3,18 +3,22 @@ package de.jlo.talendcomp.hac.impex;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.sap.hybris.hac.impex.Impex;
 import com.sap.hybris.hac.impex.ImpexResult;
 
 import de.jlo.talendcomp.hac.HybrisHac;
+import de.jlo.talendcomp.hac.TypeUtil;
 
 public class ImpexExportRunner extends HybrisHac {
 
 	private String impexHeaderWithFX = null;
 	private ImpexResult impexResult = null;
 	private String outputZipFile = null;
+	private List<ImpexImportError> importErrors = new ArrayList<>();
+	private List<String> impexErrorResponseLines = null;
 	
 	public void execute() throws Exception {
 		if (outputZipFile == null || outputZipFile.trim().isEmpty()) {
@@ -33,18 +37,34 @@ public class ImpexExportRunner extends HybrisHac {
 		} catch (Exception e) {
 			throw new Exception("Execute impex export failed: " + e.getMessage() + " impex-header: \n" + impexHeaderWithFX, e);
 		}
+		impexErrorResponseLines = impexResult.getErrors();
 		List<byte[]> result = impexResult.getExportResources();
-		OutputStream fout = new FileOutputStream(of);
-		try {
-			for (byte[] b : result) {
-				fout.write(b);
+		if (result != null && result.size() > 0) {
+			OutputStream fout = new FileOutputStream(of);
+			try {
+				for (byte[] b : result) {
+					fout.write(b);
+				}
+			} catch (Exception e) {
+				throw new Exception("Write result to output zip file: " + outputZipFile + " failed: " + e.getMessage(), e);
+			} finally {
+				fout.flush();
+				fout.close();
+			}	
+		}
+	}
+	
+	public void matchImportErrors() {
+		for (int e = 0, em = impexErrorResponseLines.size(); e < em; e++) {
+			String errorLine = impexErrorResponseLines.get(e);
+			if (TypeUtil.isEmpty(errorLine) == false) {
+				ImpexImportError error = new ImpexImportError();
+				error.setDataLine(null);
+				error.setErrorMessage(TypeUtil.unescapeHtmlEntities(errorLine));
+				error.setErrorLineNumber(e);
+				importErrors.add(error);
 			}
-		} catch (Exception e) {
-			throw new Exception("Write result to output zip file: " + outputZipFile + " failed: " + e.getMessage(), e);
-		} finally {
-			fout.flush();
-			fout.close();
-		}	
+		}
 	}
 
 	public String getImpexHeaderWithFX() {
